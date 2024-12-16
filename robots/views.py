f418bc0c58fa6b3b0d4b192fc3aec7807990d714
Nikita Viktorov <nikita.viktorov088@gmail.com>
2024-12-16 19:32:20 +0300

    добавил функционал task-2

--- robots/views.py
+++ robots/views.py
@@ -1,6 +1,7 @@
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.views import View
 from robots.forms import RobotForm
+from robots.utils import generate_report_robots
 
 
 class RobotView(View):
@@ -12,3 +13,13 @@ class RobotView(View):
             form.save()
             return JsonResponse({'status': 'ok', 'message': 'Робот добавлен!'})
         return JsonResponse({'status': 'error'})
+
+class DownloadReportView(View):
+    @staticmethod
+    def get(request, *args, **kwargs):
+        file_path = generate_report_robots()
+        with open(file_path, 'rb') as f:
+            response = HttpResponse(f.read(),
+                                    content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+            response['Content-Disposition'] = f'attachment; filename="robot.xlsx"'
+            return response
